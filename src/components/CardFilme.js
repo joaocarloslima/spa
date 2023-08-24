b9f63b6dd795e8c85275be3f8de79f351cc4c431
Joao <joaocarloslima@me.com>
2023-08-24 09:58:39 -0300

    aula03

--- src/components/CardFilme.js
+++ src/components/CardFilme.js
@@ -1,9 +1,29 @@
+"use client"
+
+import { useFavoritos } from '@/hooks/useFavoritos';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import { Star } from "lucide-react";
+import { useEffect } from "react";
 
 export default function CardFilme({filme}) {
-    const { titulo, nota, poster } = filme;
+    const { titulo, nota, poster } = filme
+    const {favorito, favoritar, desfavoritar, carregarDados } = useFavoritos(filme)
+
+    useEffect(carregarDados, [])
+
     return (
-        <div className="flex flex-col items-center w-40">
+        <div className="flex flex-col items-center w-40 relative">
+            {favorito ? 
+                <FavoriteIcon 
+                    className="text-rose-600 absolute right-1 top-1 cursor-pointer" 
+                    onClick={desfavoritar}
+                />:
+                <FavoriteBorderIcon 
+                    className="absolute right-1 top-1 cursor-pointer" 
+                    onClick={favoritar}
+                />
+            }
             <img className="rounded" src={poster} alt="poster do filme" />
             <span className="text-center line-clamp-1">
                 {titulo}
